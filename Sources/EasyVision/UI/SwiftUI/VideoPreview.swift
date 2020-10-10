151def53bd3b3bb4e9117b2502216d2d11d7ef2d
Joshua Sullivan <joshuasullivan@gmail.com>
2020-10-10 10:32:47 -0500

    Troubleshooting video orientation in SwiftUI.

--- Sources/EasyVision/UI/SwiftUI/VideoPreview.swift
+++ Sources/EasyVision/UI/SwiftUI/VideoPreview.swift
@@ -7,14 +7,21 @@ public struct VideoPreview: UIViewRepresentable {
 
     let session: AVCaptureSession
 
-    let preview: VideoPreviewView = VideoPreviewView(frame: .zero)
+    let preview: VideoPreviewView = {
+        let preview = VideoPreviewView(frame: .zero)
+        preview.translatesAutoresizingMaskIntoConstraints = false
+        return preview
+    }
 
     public func makeUIView(context: Context) -> VideoPreviewView {
         preview.videoPreviewLayer.session = session
+        print("connection: \(preview.videoPreviewLayer.connection)")
+        preview.videoPreviewLayer.videoGravity = .resizeAspectFill
         return preview
     }
 
     public func updateUIView(_ uiView: VideoPreviewView, context: Context) {
         // Nothing, yet.
+        print("VideoPreview.updateUIView")
     }
 }
