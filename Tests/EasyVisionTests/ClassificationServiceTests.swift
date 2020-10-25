0a101e1d4f406bb5f6966be2932c9c22d972b517
Joshua Sullivan <joshuasullivan@gmail.com>
2020-10-25 14:12:59 -0500

    Created some ClassificationService tests, but they're not being run for some reason.

--- Tests/EasyVisionTests/ClassificationServiceTests.swift
+++ Tests/EasyVisionTests/ClassificationServiceTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import Combine
 @testable import EasyVision
 
-final class ClassificatonServiceTests: XCTest {
+class ClassificatonServiceTests: XCTest {
 
     var model: MockMLModel!
     var subject: ClassificationService!
@@ -15,17 +15,19 @@ final class ClassificatonServiceTests: XCTest {
         subject = ClassificationService(model: model)
         classifications = []
         image = CIImage(color: .blue).cropped(to: CGRect(x: 0, y: 0, width: 640, height: 480))
+        subscription = nil
     }
 
     func testClassify() {
+        let valueExpectation = XCTestExpectation(description: "Wait for a value to be received from the classification service.")
         subscription = subject.classifications.sink(
             receiveCompletion: { result in
                 XCTFail("The `classifications` publisher completed with an error.")
             },
             receiveValue: { classifications in
-                self.classifications = classifications
+                XCTAssertFalse(classifications.isEmpty)
+                valueExpectation.fulfill()
             })
-        let valueExpectation = XCTestExpectation(description: "Wait for a value to be received from the classification service.")
         subject.classify(image: image, orientation: .up)
         let result = XCTWaiter().wait(for: [valueExpectation], timeout: 1.0)
         XCTAssertEqual(result, .completed)
